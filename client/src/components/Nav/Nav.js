b44ac325926444ebcb2ba74af42dcb88964c84ad
ADC04 <allendcooper@gmail.com>
2019-09-17 20:05:23 -0400

    created Dashboard.js page; created Nav.js component with logout button to render on Dashboard page; logout now working

---
+++ client/src/components/Nav/Nav.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import "./nav.css"
+import Axios from "axios";
+
+class Nav extends Component {
+
+  state = {
+    redirectTo: null
+  }
+
+  handleLogout = event => {
+    event.preventDefault();
+    Axios.post("/user/logout")
+    .then(response => {
+      console.log(response.data)
+      if (response.data === 200) {
+        this.props.updateUser({
+          loggedIn: false,
+          username: null
+        })
+      }
+      this.setState({
+        redirectTo: "/"
+      })
+    }).catch(error => {
+      console.log("Logout error")
+    })
+  }
+  
+  render() {
+    if (this.state.redirectTo) {
+      return <Redirect to={{ pathname: this.state.redirectTo }} />
+    } else {
+      return (
+        <nav>
+          <ul>
+            <li><a href="/dashboard">dashboard</a></li>
+            <li><a href="/search">search</a></li>
+            <li><a id="logout" onClick={this.handleLogout} href="#">logout</a></li>
+          </ul>
+        </nav>
+      );
+    };
+  };
+};
+
+export default Nav;
