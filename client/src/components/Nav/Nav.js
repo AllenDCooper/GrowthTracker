da1304c327a9a583217370bf218ee8f5cf43e52c
herrenchek <giselle.herr@icloud.com>
2019-09-21 11:45:42 -0400

    Create added to navbar

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -37,6 +37,7 @@ class Nav extends Component {
           <ul>
             <li><a href="/dashboard">dashboard</a></li>
             <li><a href="/search">search</a></li>
+            <li><a href="/create">create</a></li>
             <li><a id="logout" onClick={this.handleLogout} href="#">logout</a></li>
           </ul>
         </nav>
