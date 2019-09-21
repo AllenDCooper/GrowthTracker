f212e7006d1f7ed971700e2a56da584fd480b9c1
herrenchek <giselle.herr@icloud.com>
2019-09-21 12:34:38 -0400

    Updated form

--- client/src/pages/Create.js
+++ client/src/pages/Create.js
@@ -26,20 +26,25 @@ class Create extends Component {
           <div className="row">
             <form className="col s12">
               <div className="input-field col s12">
-                <input id="survey_title" name="surveyTitle" type="text" className="validate" />
+                <input id="survey_title" name="surveyTitle" type="text" data-length="20" className="validate" />
                 <label for="survey_title">Survey Title</label>
               </div>
               <div className="input-field col s12">
-                <textarea id="textarea1" name="surveyDescription" className="materialize-textarea"></textarea>
+                <textarea id="textarea1" name="surveyDescription" className="materialize-textarea" data-length="120"></textarea>
                 <label for="textarea1">Description</label>
+              </div>
+              <div className="input-field inline col s12">
+                <i class="material-icons prefix">mode_edit</i>
+                <input id="email_inline" type="email" className="validate" />
+                <label for="email_inline">Question 1</label>
               </div>
             </form>
           </div>
+          </div>
         </div>
-      </div>
-    )
-  }
-
-}
-
+        )
+      }
+    
+    }
+    
 export default Create;
