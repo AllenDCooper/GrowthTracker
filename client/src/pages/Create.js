bfba31c752d0c49f92c4b178b3678e186ff862c8
herrenchek <giselle.herr@icloud.com>
2019-09-20 21:55:02 -0400

    Added form to create

--- client/src/pages/Create.js
+++ client/src/pages/Create.js
@@ -1,26 +1,47 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Nav from "../components/Nav/Nav.js"
 
 class Create extends Component {
-    state = {
-        question1: "",
-        question2: "",
-        question3: "",
-        question4: "",
-        question5: "",
-        question6: "",
-        question7: "",
-        question8: "",
-        question9: "",
-        question10: "",
-    }
+  state = {
+    surveyTitle: "",
+    surveyDescription: "",
+    question1: "",
+    question2: "",
+    question3: "",
+    question4: "",
+    question5: "",
+    question6: "",
+    question7: "",
+    question8: "",
+    question9: "",
+    question10: "",
+  }
 
-    render() {
-        return (
-            <div>
-
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Nav {...this.props} updateUser={this.props.updateUser} loggedIn={this.props.loggedIn} />
+        <div className="container">
+          <div className="row">
+            <form className="col s12">
+              <div className="input-field col s12">
+                <input id="survey_title" name="surveyTitle" type="text" className="validate" />
+                <label for="survey_title">Survey Title</label>
+              </div>
+              <div className="row">
+                <div className="input-field col s12">
+                  <textarea id="textarea1" name="surveyDescription" className="materialize-textarea"></textarea>
+                  <label for="textarea1">Description</label>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    )
+  }
 
 }
+
+export default Create;
